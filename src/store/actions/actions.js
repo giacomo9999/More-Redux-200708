23cf10ba2b34cc96d288b1766e77d9fd9d90bdf3
giacomo9999 <gmarchesi888@gmail.com>
2020-07-07 16:29:45 -0400

    Added simple asynchronous action.

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -21,8 +21,16 @@ export const subtract = () => {
   return { type: SUBTRACT, payload: 5 };
 };
 
-export const storeResult = (result) => {
-  return { type: STORE_RESULT, result: result };
+export const saveResult = (res) => {
+  return { type: STORE_RESULT, result: res };
+};
+
+export const storeResult = (res) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(saveResult(res));
+    }, 2000);
+  };
 };
 
 export const deleteResult = (elementId) => {
